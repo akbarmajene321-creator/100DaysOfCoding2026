02e5f79c775e47c4f2a15dd76625372a00d98975
Akbar <akbarmajene321@gmail.com>
2026-09-02 23:06:38 +0800

    Add Main class with comments and main method

---
+++ Day1.java
@@ -0,0 +1,16 @@
+public class Main {
+    // ini adalah komentar biasa 
+    
+    /*ini adalah komentar multiline
+     * komentar ini dapat memberikan komentar yang lebih dari 1 baris
+     */
+     
+    /** ini adalah komentar javadoc
+     * 
+     */
+    public static void main(String[] args) {
+        System.out.println("perkenalkan nama saya akbar");
+        System.out.println("saya tinggal di majene");
+        System.out.println("saya alumni dari UPTD Smkn 1 Majene");
+    }
+}
